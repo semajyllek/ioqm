af1e336c91c35ae08368984fa25957bf6723e9cf
James Kelly <jkelly@sensoryinc.com>
2023-10-09 21:08:33 -0700

    trying guidance changes

--- run_gen.py
+++ run_gen.py
@@ -87,8 +87,8 @@ def get_img_gen_pipe(model_id: str) -> Any:
 			pipe,
 			height=1024,
 			width=1024,
-			prior_guidance_scale=9.0,
-			decoder_guidance_scale=4.0,
+			prior_guidance_scale=4.0,
+			decoder_guidance_scale=0.0,
 		)
 		
 	else:
